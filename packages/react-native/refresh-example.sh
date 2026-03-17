c2d6f4b2180c7ca7b26c00910ef0f2be17ffef9e
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2026-03-17 21:16:48 +0530

    [ATTEMPT] Get example app working

--- packages/react-native/refresh-example.sh
+++ packages/react-native/refresh-example.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-echo "Please ensure you run this from the packages/react-native directory inside the cloned invertase/notifee repo"
+echo "Please ensure you run this from the packages/react-native directory inside the cloned psync/notifee repo"
 echo "You should only do this when your git working set is completely clean (e.g., git reset --hard)"
-echo "You must have already run \`yarn\` in the repository so \`npx react-native\` will work"
+echo "You must have already run \`bun install\` in the repository so CLI tools work"
 echo "This scaffolding refresh has been tested on macOS, not on windows or linux"
 
 # Copy the important files out temporarily
@@ -23,44 +23,49 @@ fi
 # Purge the old sample
 \rm -fr example
 
-# Make the new example
+# Make the new example using the modern Community CLI and bun
 echo "Creating example app"
-npx react-native init example --template react-native-template-typescript
+npx @react-native-community/cli@latest init example --pm bun
 pushd example
 
 # Update package.json
 echo "Adding Notifee app package"
 # add notifee dependency
 npx json -I -f package.json -e 'this.dependencies["@psync/notifee"] = "file:../"'
-# add post install step for notifee
-npx json -I -f package.json -e 'this.scripts.postinstall = "cd node_modules/@psync/notifee && yarn"'
+# add post install step for notifee using bun
+npx json -I -f package.json -e 'this.scripts.postinstall = "cd node_modules/@psync/notifee && bun install"'
 
-# Update compileSdkVersion and targetSdkVersion to 33
+# Update SDK Versions (React Native 0.83+ / Android 16 targets API 36)
 echo "Updating android/build.gradle"
-sed -i -e $'s/ 31/ 33/' android/build.gradle
-rm -f android/build.gradle??
+sed -i "" -e 's/compileSdkVersion = [0-9]*/compileSdkVersion = 36/' android/build.gradle
+sed -i "" -e 's/targetSdkVersion = [0-9]*/targetSdkVersion = 36/' android/build.gradle
 
 echo "Updating AndroidManifest.xml"
 sed -i "" -e $'s/android:name=".MainActivity"/android:name=".MainActivity"\\\n      android:showWhenLocked="true"\\\n        android:turnScreenOn="true"/;s/\<\/activity\>/\<\/activity\>\\\n      \<activity\\\n        android:name="com.example.CustomActivity"\\\n      android:showWhenLocked="true"\\\n      android:turnScreenOn="true"\\\n    \/\>/' android/app/src/main/AndroidManifest.xml
 
 echo "Updating MainActivity.java"
-sed -i "" -e $'s/package com.example;/package com.example;\\\nimport io.invertase.notifee.NotifeeApiModule;/;s/return "example"/return NotifeeApiModule.getMainComponent("example")/' android/app/src/main/java/com/example/MainActivity.java
+# Note: Modern RN generates MainActivity in kotlin (.kt), but keeping .java fallback just in case
+if [ -f android/app/src/main/java/com/example/MainActivity.kt ]; then
+  sed -i "" -e $'s/package com.example/package com.example\\\nimport io.invertase.notifee.NotifeeApiModule/;s/return "example"/return NotifeeApiModule.getMainComponent("example")/' android/app/src/main/java/com/example/MainActivity.kt
+elif [ -f android/app/src/main/java/com/example/MainActivity.java ]; then
+  sed -i "" -e $'s/package com.example;/package com.example;\\\nimport io.invertase.notifee.NotifeeApiModule;/;s/return "example"/return NotifeeApiModule.getMainComponent("example")/' android/app/src/main/java/com/example/MainActivity.java
+fi
 
 echo "Updating iOS Podfile"
 # This is just a speed optimization, very optional, but asks xcodebuild to use clang and clang++ without the fully-qualified path
-# That means that you can then make a symlink in your path with clang or clang++ and have it use a different binary
-# In that way you can install ccache or buildcache and get much faster compiles...
-sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["CC"] = "clang"\\\n        config.build_settings["LD"] = "clang"\\\n        config.build_settings["CXX"] = "clang++"\\\n        config.build_settings["LDPLUSPLUS"] = "clang++"\\\n      end\\\n    end/' ios/Podfile
-rm -f ios/Podfile??
+sed -i "" -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["CC"] = "clang"\\\n        config.build_settings["LD"] = "clang"\\\n        config.build_settings["CXX"] = "clang++"\\\n        config.build_settings["LDPLUSPLUS"] = "clang++"\\\n      end\\\n    end/' ios/Podfile
 
 echo "Installing pods"
-npx pod-install
+pushd ios
+bundle install || echo "Bundle install failed, continuing anyway..."
+bundle exec pod install || pod install
+popd
 
 # We use typescript and there are linter collisions with transitive dependencies on old versions
 # Merge the result of a PR we made upstream so lint is clean even with our 3-deep layer of packages
-yarn add --dev @react-native-community/eslint-config@^3
+bun add -d @react-native-community/eslint-config@^3
 
-yarn
+bun install
 
 # Copy the important files back in
 popd
